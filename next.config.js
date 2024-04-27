1c5d66eb740d9d8493f6c65d8175d5e96724d3e7
Michael Jansen <michaeldjansen35@gmail.com>
2024-04-27 20:24:36 +0200

    Fix asset path

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/personal-site",
+    assetPrefix: "https://michaeldjansen.github.io/personal-site",
     output: "export", 
     reactStrictMode: true,
-  };
+};
   
-  module.exports = nextConfig;
+module.exports = nextConfig;
